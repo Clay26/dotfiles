ae97938cabba8ab54fcbea1ff79d849a66562327
clay <5711623+Clay26@users.noreply.github.com>
2024-04-06 13:01:09 -0400

    Move the sets to a separate file

--- nvim/.config/nvim/lua/clay/init.lua
+++ nvim/.config/nvim/lua/clay/init.lua
@@ -1,27 +1,3 @@
 require("clay.remap")
+require("clay.set")
 
--- Editor options
-vim.o.number = true
-vim.o.relativenumber = true
-vim.o.expandtab = true
-vim.o.tabstop = 2
-vim.o.softtabstop = 2
-vim.o.shiftwidth = 2
-vim.cmd [[syntax enable]]
-vim.o.laststatus = 2
-vim.o.ruler = true
-vim.o.title = true
-vim.o.history = 1000
-vim.o.autoindent = true
-vim.o.smartindent = true
-vim.o.textwidth = 80
-vim.o.colorcolumn = "+1"
-
--- Open new split panes to right and bottom, which feels more natural
-vim.o.splitbelow = true
-vim.o.splitright = true
-
--- Search options
-vim.o.hlsearch = true
-vim.o.ignorecase = true
-vim.o.smartcase = true
---
+++ nvim/.config/nvim/lua/clay/set.lua
@@ -0,0 +1,38 @@
+-- Editor options
+vim.o.number = true
+vim.o.relativenumber = true
+
+-- Tab spacing options
+vim.o.expandtab = true
+vim.o.tabstop = 2
+vim.o.softtabstop = 2
+vim.o.shiftwidth = 2
+
+vim.o.autoindent = true
+vim.o.smartindent = true
+
+vim.o.wrap = true
+
+vim.o.swapfile = false
+vim.o.backup = false
+vim.o.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.o.undofile = true
+
+vim.cmd [[syntax enable]]
+vim.o.laststatus = 2
+vim.o.ruler = true
+vim.o.title = true
+vim.o.history = 1000
+vim.o.textwidth = 80
+vim.o.colorcolumn = "+1"
+
+-- Open new split panes to right and bottom, which feels more natural
+vim.o.splitbelow = true
+vim.o.splitright = true
+
+-- Search options
+vim.o.hlsearch = true
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+vim.o.termguicolors = true
