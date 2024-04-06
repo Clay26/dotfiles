10abb61f756ca49d79b285afad4bdbd529b60bbd
clay <5711623+Clay26@users.noreply.github.com>
2024-04-06 13:14:57 -0400

    Add some primeagean remaps

--- nvim/.config/nvim/lua/clay/remap.lua
+++ nvim/.config/nvim/lua/clay/remap.lua
@@ -1,9 +1,29 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
+-- Move lines selected in visual mode
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Keep cursor at same spot when using jon
+vim.keymap.set("n", "J", "mzJ`z")
+
+-- Keep cursor in middle when navigating search results
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- Delete word to void register to maintain paste register
+vim.keymap.set("x", "<leader>p", [["_dP]])
+
+-- Turn off Q
+vim.keymap.set("n", "Q", "<nop>")
+
+-- Start sub of current word
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
 -- Turn off arrow keys
-vim.api.nvim_set_keymap('', '<Left>', '<nop>', {noremap = true})
-vim.api.nvim_set_keymap('', '<Right>', '<nop>', {noremap = true})
-vim.api.nvim_set_keymap('', '<Up>', '<nop>', {noremap = true})
-vim.api.nvim_set_keymap('', '<Down>', '<nop>', {noremap = true})
+vim.keymap.set("", "<Left>", "<nop>")
+vim.keymap.set("", "<Right>", "<nop>")
+vim.keymap.set("", "<Up>", "<nop>")
+vim.keymap.set("", "<Down>", "<nop>")
 
