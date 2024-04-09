df9f811512d7023e20ba302e0b16c9ed1dc2b335
clay <5711623+Clay26@users.noreply.github.com>
2024-04-09 17:48:46 -0400

    Update powershell profiles

--- powershell/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ powershell/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,7 +1,48 @@
+# Variables
+$DOTS_DIR = "$HOME\Projects\dotfiles"
+$CONFIG = "$DOTS_DIR\powershell\WindowsPowerShell\Microsoft.PowerShell_profile.ps1"
+
 # Chocolatey profile
 $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
 if (Test-Path($ChocolateyProfile)) {
   Import-Module "$ChocolateyProfile"
 }
 
-oh-my-posh init powershell --config 'C:/Users/cljackso/Posh/cljackso.omp.json' | Invoke-Expression
+#oh-my-posh init powershell --config 'C:/Users/cljackso/Posh/cljackso.omp.json' | Invoke-Expression
+
+# Define functions for the commands
+function Open-Vim { nvim }
+function Show-GitStatus { git status }
+function GoTo-Dots { Set-Location -Path $DOTS_DIR }
+
+# Create aliases for these functions
+if (-not (Get-Alias -Name vim -ErrorAction SilentlyContinue)) {
+    New-Alias -Name vim Open-Vim
+}
+if (-not (Get-Alias -Name gs -ErrorAction SilentlyContinue)) {
+	New-Alias -Name gs Show-GitStatus
+}
+if (-not (Get-Alias -Name dots -ErrorAction SilentlyContinue)) {
+	New-Alias -Name dots GoTo-Dots
+}
+
+function Copy-ProfileToLocation {
+    $SourcePath = $CONFIG
+
+    try {
+
+        # Check if the source file exists
+        if (Test-Path $SourcePath) {
+            # Copy the profile to the $PROFILE location
+            Copy-Item -Path $SourcePath -Destination $PROFILE -Force
+            Write-Host "Profile copied successfully to $PROFILE"
+	    
+	    # Source PROFILE
+	    . $PROFILE
+        } else {
+            Write-Host "The source profile path does not exist."
+        }
+    } catch {
+        Write-Host "An error occurred: $_"
+    }
+}
--- pwsh/PowerShell/Microsoft.PowerShell_profile.ps1
+++ pwsh/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,7 @@
+# Variables
+$DOTS_DIR = "$HOME\Projects\dotfiles"
+$CONFIG = "$DOTS_DIR\pwsh\PowerShell\Microsoft.PowerShell_profile.ps1"
+
 # Chocolatey profile
 $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
 if (Test-Path($ChocolateyProfile)) {
@@ -5,3 +9,40 @@ if (Test-Path($ChocolateyProfile)) {
 }
 
 oh-my-posh init pwsh --config 'C:/Users/cljackso/Posh/cljackso.omp.json' | Invoke-Expression
+
+# Define functions for the commands
+function Open-Vim { nvim }
+function Show-GitStatus { git status }
+function GoTo-Dots { Set-Location -Path $DOTS_DIR }
+
+# Create aliases for these functions
+if (-not (Get-Alias -Name vim -ErrorAction SilentlyContinue)) {
+    New-Alias -Name vim Open-Vim
+}
+if (-not (Get-Alias -Name gs -ErrorAction SilentlyContinue)) {
+	New-Alias -Name gs Show-GitStatus
+}
+if (-not (Get-Alias -Name dots -ErrorAction SilentlyContinue)) {
+	New-Alias -Name dots GoTo-Dots
+}
+
+function Copy-ProfileToLocation {
+    $SourcePath = $CONFIG
+
+    try {
+
+        # Check if the source file exists
+        if (Test-Path $SourcePath) {
+            # Copy the profile to the $PROFILE location
+            Copy-Item -Path $SourcePath -Destination $PROFILE -Force
+            Write-Host "Profile copied successfully to $PROFILE"
+	    
+	    # Source PROFILE
+	    . $PROFILE
+        } else {
+            Write-Host "The source profile path does not exist."
+        }
+    } catch {
+        Write-Host "An error occurred: $_"
+    }
+}
