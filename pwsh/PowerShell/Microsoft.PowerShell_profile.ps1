4e2b98d55bcf65d0b5961e94a91ff76d78b84b05
clay <5711623+Clay26@users.noreply.github.com>
2024-04-15 14:15:39 -0400

    Add Ensure clang in path powershell function

--- pwsh/PowerShell/Microsoft.PowerShell_profile.ps1
+++ pwsh/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -73,3 +73,38 @@ function Export-WingetList {
   $ExportFile = "$DOTS_DIR\winget_file"
   winget list --source winget | Out-File -FilePath $ExportFile
 }
+
+function Ensure-ClangInPath {
+    # Check if 'clang.exe' is in the PATH
+    $clangInPath = $false
+    $clangPath = "C:\Program Files\LLVM\bin"
+    $backupPathFile = "$DOTS_DIR\path.bak"
+    $path = [Environment]::GetEnvironmentVariable("Path", [EnvironmentVariableTarget]::User)
+
+    # Splitting the path to check each directory
+    $directories = $path.Split(';')
+    foreach ($dir in $directories) {
+        if (Test-Path "$dir\clang.exe") {
+            $clangInPath = $true
+            break
+        }
+    }
+
+    # Proceed if clang is not in PATH
+    if (-not $clangInPath) {
+        if (Test-Path "$clangPath\clang.exe") {
+            # Back up current path to local file
+            $backupFilePath = "$env:USERPROFILE\path.bak"
+            $path | Out-File -FilePath $backupPathFile
+
+            # Adding LLVM bin to PATH
+            $newPath = "$path;$clangPath"
+            [Environment]::SetEnvironmentVariable("Path", $newPath, [EnvironmentVariableTarget]::User)
+            Write-Host "Added clang to PATH. Please restart PowerShell to apply changes."
+        } else {
+            Write-Host "Error: clang is not installed at $clangPath."
+        }
+    } else {
+      Write-Host "clang is already in the PATH."
+    }
+}
